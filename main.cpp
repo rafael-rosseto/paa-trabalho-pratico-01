545081358d93344d717a6828c87d4e76aabe8720
Rafael Rosseto <rafael@arch.localdomain>
2021-01-18 23:10:19 -0300

    driver function for testing

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 #include "vetor.h"
 #include "ordenacao.h"
 
+using namespace std;
+
 #define COUNT_OF(x) ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
 
-int vetorA[1000], vetorB[5000], vetorC[25000];
+int vetorA[30], vetorB[5000], vetorC[25000];
 
 int main()
 {
-    gerarVetor(vetorA, 1, COUNT_OF(vetorA));
+    gerarVetor(vetorA, false, COUNT_OF(vetorA));
+    cout << "Vetor desordenado:" << endl;
+    imprimirVetor(vetorA, COUNT_OF(vetorA));
+    selectionSort(vetorA, COUNT_OF(vetorA));
+    cout << "Vetor ordenado:" << endl;
     imprimirVetor(vetorA, COUNT_OF(vetorA));
 
     return 0;
